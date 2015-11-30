21b41b7dacd64d77be7dfdd4c044af5523b71198
Dave Anderson (xTuple) <danderson@xtuple.com>
2015-11-30 16:34:07 +1300

    Minor tweak to allow <comment> tag in package.xml files

--- common/package.cpp
+++ common/package.cpp
@@ -150,6 +150,9 @@ Package::Package(const QDomElement & elem, QStringList &msgList,
       _finalscripts.append(new FinalScript(elemThis, msgList, fatalList));
     else if(elemThis.tagName() == "initscript")
       _initscripts.append(new InitScript(elemThis, msgList, fatalList));
+    else if(elemThis.tagName() == "comment")
+      // Package Comment - Do nothing
+      bool comments = true;  
     else if (! reportedErrorTags.contains(elemThis.tagName()))
     {
       if (handler)
