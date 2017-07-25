0f375a995f6d500bbe53602a41ca1c7cafd4554d
Chris Cotnoir <ccotnoir@xtuple.com>
2017-07-25 12:55:28 -0400

    Add translation package object

--- common/loadqm.cpp
+++ common/loadqm.cpp
@@ -7,116 +7,122 @@
 #include <iostream>
 #include <QString>
 
-
-
 LoadQm::LoadQm(const QString &name, const int grade, const bool system, const QString &comment, const QString &filename)
-	: Loadable("loadqm", name, grade, system, comment, filename)
+       : Loadable("loadqm", name, grade, system, comment, filename)
 {
-	_pkgitemtype = "Q";
+  _pkgitemtype = "Q";
 }
 
 LoadQm::LoadQm(const QDomElement & elem, const bool system, QStringList &msg, QList<bool> &fatal)
-	: Loadable(elem, system, msg, fatal)
+       : Loadable(elem, system, msg, fatal)
 {
-	_pkgitemtype = "Q";
+  _pkgitemtype = "Q";
 
-	if (elem.nodeName() != "loadqm")
-	{
-		msg.append(TR("Creating a LoadQm element from a %1 node.")
-			.arg(elem.nodeName()));
-		fatal.append(false);
-	}
+  if (elem.nodeName() != "loadqm")
+  {
+    msg.append(TR("Creating a LoadQm element from a %1 node.")
+                 .arg(elem.nodeName()));
+    fatal.append(false);
+  }
 }
 
 int LoadQm::writeToDB(const QByteArray &pdata, const QString pkgname, QString &errMsg)
 {
-	QString extension_name = "";
-	QString country = "";
-	QString lang = "";
+  QString country = "";
+  QString lang = "";
+  int langid = -1;
+  int countryid = -1;
 
+  QStringList directory_chop = _filename.split("/");
+  QString formatted_filename = directory_chop[directory_chop.size() - 1];
 
-	QStringList directory_chop = _filename.split("/");
-	QString formatted_filename = directory_chop[directory_chop.size() - 1];
+  QStringList file_parts = formatted_filename.split(".");
+  QStringList locale_parts = file_parts[1].split("_");
 
-	QStringList file_parts = formatted_filename.split(".");
-	QStringList locale_parts = file_parts[1].split("_");
-	//file_parts[0] == "extension", file_parts[1] == "lang_country"
-	//locale_parts[0] == "lang", locale_parts[1] == "country"
+  lang = locale_parts[0];
+  if (locale_parts.size() > 1)
+  {
+    country = locale_parts[1];
+  }
 
-	extension_name = file_parts[0];
-	lang = locale_parts[0];
-	if (locale_parts.size() > 1) {
-		country = locale_parts[1];
-	}
+  XSqlQuery getids;
+  getids.prepare("SELECT lang_id, country_id "
+                 "  FROM lang, country "
+                 " WHERE lang_abbr2=:lang "
+                 "   AND (country_abbr=:country OR :country IS NULL);");
+  getids.bindValue(":lang", lang);
+  if (!country.isEmpty())
+    getids.bindValue(":country", country.toUpper());
+  getids.exec();
+  if (getids.first())
+  {
+    langid = getids.value("lang_id").toInt();
+    if (!country.isEmpty())
+      countryid = getids.value("country_id").toInt();
+  }
+  else if (getids.lastError().type() != QSqlError::NoError)
+  {
+    QSqlError err = getids.lastError();
+    errMsg = err.databaseText();
+    return -1;
+  }
 
+  QString version;
+  if (pkgname.isEmpty())
+  {
+    XSqlQuery getver("SELECT fetchmetrictext('ServerVersion') AS version;");
+    if (getver.first())
+      version = getver.value("version").toString();
+    else if (getver.lastError().type() != QSqlError::NoError)
+    {
+      QSqlError err = getver.lastError();
+      errMsg = err.databaseText();
+      return -1;
+    }
+  }
+  else
+  {
+    XSqlQuery getver;
+    getver.prepare("SELECT pkghead_version "
+                   "  FROM pkghead "
+                   " WHERE pkghead_name=:name;");
+    getver.bindValue(":name", pkgname);
+    getver.exec();
+    if (getver.first())
+      version = getver.value("pkghead_version").toString();
+    else if (getver.lastError().type() != QSqlError::NoError)
+    {
+      QSqlError err = getver.lastError();
+      errMsg = err.databaseText();
+      return -1;
+    }
+  }
 
+  _selectMql = new MetaSQLQuery("SELECT dict_id "
+                                "  FROM ONLY <? literal('tablename') ?> "
+                                " WHERE dict_lang_id=<? value('lang') ?> "
+                                "   AND dict_country_id=<? value('country') ?>;");
 
-        QString version;
-        if (pkgname.isEmpty())
-        {
-          XSqlQuery getver("SELECT fetchmetrictext('ServerVersion') AS version;");
-          if (getver.first())
-            version = getver.value("version").toString();
-          else if (getver.lastError().type() != QSqlError::NoError)
-          {
-            QSqlError err = getver.lastError();
-            errMsg = err.databaseText();
-            return -1;
-          }
-        }
-        else
-        {
-          XSqlQuery getver;
-          getver.prepare("SELECT pkghead_version "
-                         "  FROM pkghead "
-                         " WHERE pkghead_name=:name;");
-          getver.bindValue(":name", pkgname);
-          getver.exec();
-          if (getver.first())
-            version = getver.value("pkghead_version").toString();
-          else if (getver.lastError().type() != QSqlError::NoError)
-          {
-            QSqlError err = getver.lastError();
-            errMsg = err.databaseText();
-            return -1;
-          }
-        }
+  _updateMql = new MetaSQLQuery("UPDATE <? literal('tablename') ?> "
+                                "   SET dict_data=<? value('data') ?>, "
+                                "       dict_version=<? value('version') ?> "
+                                " WHERE dict_id=<? value('id') ?> "
+                                "RETURNING dict_id AS id;");
 
-	//give the 0 pointer a MetaSQLQuery value
+  _insertMql = new MetaSQLQuery("INSERT INTO <? literal('tablename') ?> "
+                                "(dict_lang_id, dict_country_id, "
+                                " dict_data, dict_version) "
+                                "VALUES (<? value('lang') ?>, <? value('country') ?>, "
+                                " <? value('data') ?>, <? value('version') ?>) "
+                                "RETURNING dict_id AS id;");
 
-        _selectMql = new MetaSQLQuery("SELECT qm_id "
-                                      "  FROM qm "
-                                      " WHERE qm_extension_name=<? value('extension_nm') ?> "
-                                      "   AND qm_lang=<? value('lang') ?> "
-                                      "   AND qm_country=<? value('country') ?>;");
+  ParameterList params;
+  params.append("tablename", "dict");
+  params.append("lang", langid);
+  if (!country.isEmpty())
+    params.append("country", countryid);
+  params.append("data", QVariant(pdata));
+  params.append("version", version);
 
-        _updateMql = new MetaSQLQuery("UPDATE qm "
-                                      "   SET qm_data=<? value('qm_data') ?>, "
-                                      "       qm_version=<? value('version') ?> "
-                                      "RETURNING qm_id AS id;");
-
-	_insertMql = new MetaSQLQuery("INSERT INTO qm "
-                                      "(qm_extension_name, qm_lang, "
-                                      " qm_country, qm_data, "
-                                      " qm_version) "
-                                      "VALUES (<? value('extension_nm') ?>, <? value('lang') ?>, "
-                                      "        <? value('country') ?>, <? value('qm_data') ?>, "
-                                      "        <? value('version') ?>) "
-                                      "RETURNING qm_id AS id;");
-
-	//assign params
-	ParameterList params;
-	params.append("extension_nm", extension_name);
-	params.append("lang", lang);
-	params.append("country", country);
-	params.append("qm_data", QVariant(pdata));
-        params.append("version", version);
-
-
-	//let Loadable::writeToDB handle the execution of the _insertMql query
-	return Loadable::writeToDB(pdata, pkgname, errMsg, params);
-
-
-
-
+  return Loadable::writeToDB(pdata, pkgname, errMsg, params);
 }
